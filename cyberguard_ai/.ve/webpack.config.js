093a81f75f65caf4866a55fbf33ae7be57e0c4f6
Kavia AI <production@kavia.ai>
2025-06-14 09:02:22 +0000

    CheckPoint

--- cyberguard_ai/.ve/webpack.config.js
+++ cyberguard_ai/.ve/webpack.config.js
@@ -1,48 +1,48 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-// PUBLIC_INTERFACE
+const devPort = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
+
 module.exports = {
-  entry: path.resolve(__dirname, "../src/index.js"),
+  entry: path.resolve(__dirname, '../src/index.js'),
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
     clean: true,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '../src'),
+    },
+    port: devPort,
+    hot: true,
+    historyApiFallback: true,
+    allowedHosts: 'all', // Allow all hosts to resolve 'Invalid Host Header'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: "babel-loader",
+        use: {
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "../public/index.html"),
-      favicon: path.resolve(__dirname, "../public/favicon.ico"),
-      inject: "body",
+      template: path.resolve(__dirname, '../src/index.html'),
+      inject: 'body',
     }),
   ],
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "../public"),
-    },
-    port: process.env.PORT || 3001, // Default to 3001 for dev
-    hot: true,
-    historyApiFallback: true,
-    open: true,
-    host: "0.0.0.0"
-  },
-  devtool: "eval-source-map",
-  mode: "development",
+  mode: 'development',
 };
